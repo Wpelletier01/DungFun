5fbe0c81999d9bc78e501259ac0807c1cc0af9f1
william <wpelletier.development@yahoo.com>
2024-10-26 13:18:59 -0400

    add registry pointer and clean method declaration

--- include/libgame/Renderer.h
+++ include/libgame/Renderer.h
@@ -6,18 +6,20 @@
 #include "entt.hpp"
 #include "World.h"
 
+using TexRegister = std::unordered_map<std::string, SDL_Texture*>&;
 
 class Renderer {
 public:
     ~Renderer();
-    int init(SDL_Window* win);
-    void clear();
-    void renderAll(entt::registry& registry, std::unordered_map<std::string, SDL_Texture*>& textures, World* world);
-    void present();
+    int init(SDL_Window* win, entt::registry* registry);
+    void clear() const;
+    void renderAll(TexRegister& textures, World* world, SDL_Rect& camera);
+    void present() const;
     void setViewport(int x, int y);
-    SDL_Renderer* getpSDLRenderer();
+    SDL_Renderer* getpSDLRenderer() const;
 private:
-    SDL_Renderer* mrenderer = nullptr;
+    SDL_Renderer* m_renderer = nullptr;
+    entt::registry* m_registry = nullptr;
 };
 
 
